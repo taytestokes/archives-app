4d50fcb6b3bddc613eddbd4b222806cfed101a2d
taytestokes <taytestokes9328@gmail.com>
2023-09-08 13:38:57 -0600

    Protects dashboard from being accessed from unauthrized sessions

--- app/(routes)/dashboard/page.js
+++ app/(routes)/dashboard/page.js
@@ -1,3 +1,12 @@
-export default function DashboardPage() {
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+
+export default async function DashboardPage() {
+  const session = await getServerSession();
+
+  if (!session) {
+    redirect("/");
+  }
+
   return <div>page</div>;
 }
